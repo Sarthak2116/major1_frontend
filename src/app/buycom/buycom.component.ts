a75fda75f6022c1408a01fcc48f4f7f5d3c8b08a
Sarthak Srivastava <sarthak321321@gmail.com>
2020-04-16 21:35:02 +0530

    Added message and dialog box

--- src/app/buycom/buycom.component.ts
+++ src/app/buycom/buycom.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { PredictComponent } from '../predict/predict.component';
 
 @Component({
   selector: 'app-buycom',
